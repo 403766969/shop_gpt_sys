f030b4de52550a2c365d42ce24317048187fedc4
403766969 <403766969@qq.com>
2020-08-05 23:58:14 +0800

    添加请求的进度条

--- src/components/content/welcome/Welcome.vue
+++ src/components/content/welcome/Welcome.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="welcome">
-    <h1>Welcome</h1>
+    <h1>Welcome!</h1>
+    <h2>欢迎使用电商后台管理系统！</h2>
   </div>
 </template>
 
@@ -11,4 +12,20 @@ export default {
 </script>
 
 <style lang="less" scoped>
+#welcome {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  h1{
+    transform: translateY(-80px);
+    font-size: 36px;
+  }
+  h2{
+    transform: translateY(-80px);
+    font-size: 28px;
+  }
+}
 </style>
--- src/network/service.js
+++ src/network/service.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 import { BASE_URL } from 'commonjs/const'
 
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 // 创建axios实例
 const instance = axios.create({
   baseURL: BASE_URL,
@@ -9,6 +12,8 @@ const instance = axios.create({
 
 // 请求拦截器
 instance.interceptors.request.use(config => {
+  // 显示进度条
+  NProgress.start()
   // 每次请求设置token
   config.headers.Authorization = window.sessionStorage.getItem('token')
   return config
@@ -18,6 +23,8 @@ instance.interceptors.request.use(config => {
 
 // 响应拦截器
 instance.interceptors.response.use(res => {
+  // 隐藏进度条
+  NProgress.done()
   return res
 }, err => {
   console.log('response', err)
